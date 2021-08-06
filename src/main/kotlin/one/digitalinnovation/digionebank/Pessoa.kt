8783f480081a43ef7e3122da394af654a77c5428
roneig <grimmronei@gmail.com>
2021-08-06 15:39:45 -0300

    Inclusão private set no atributo cpf

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,12 +1,32 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var nome:String = "ronei"
-    var cpf:String = "123.123.123-11"
+    var nome: String = "ronei"
+    //private set
+    var cpf: String = "123.123.123-11"
+    private set
+/*
+    setCpf(cpf: String ) {
+        this.cpf = cpf
+    }
+*/
+/*  Pessoa é a outer class
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
+*/
 }
 
 fun main() {
     val ronei = Pessoa()
     println(ronei)
     println(ronei.nome + " " + ronei.cpf)
+    //ronei.cpf = "123456"
+    //ronei.setCpf("123456")
+
+    ronei.nome = "ronei2"
+    println(ronei.nome + " " + ronei.cpf)
+
+    //println(ronei.Endereco().rua)
+
 }
